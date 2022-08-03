1a578f73d4ca6d64217605d7cb084787222915f1
Ryan Pisuena <Pisuenaryan@gmail.com>
2022-08-03 18:48:29 +0000

    Connect a Wallet Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
 const App = () => {
+
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("Kamusta Address:", address);
+
+  // If the user has not connected their wallet yet, 
+  if(!address) {
+    return (
+      
+      <div className="landing">
+        <h1>Welcome to SalsaDAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">  Connect your wallet
+        </button>
+      
+      </div>
+      
+    );
+  }
+
+
+  
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected, now what!</h1>
     </div>
   );
+
+  
 };
 
 export default App;
